234aea0d7aae78555b19cb6ad23314fd35e9eced
Syfhehe <shenyf0811@gmail.com>
2021-05-09 23:43:06 +0800

    第一个控制器

---
+++ myweb/api/controllers/test/hi.js
@@ -0,0 +1,18 @@
+/**
+ * Module dependencies
+ */
+
+// ...
+
+
+/**
+ * test/hi.js
+ *
+ * Hi test.
+ */
+module.exports = async function hi(req, res) {
+
+  sails.log.debug('TODO: implement');
+  return res.ok();
+
+};
