ba99339077dbfa055c498aea629a33bd41767ff9
Syfhehe <shenyf0811@gmail.com>
2021-05-12 00:44:47 +0800

    策略

--- myweb/config/routes.js
+++ myweb/config/routes.js
@@ -33,6 +33,8 @@ module.exports.routes = {
   'GET /account/password':   { action: 'account/view-edit-password' },
   'GET /account/profile':    { action: 'account/view-edit-profile' },
   'GET /test/hi':    { action: 'test/hi' },
+  'GET /test/about':    { action: 'test/about' },
+
 
   //  ╔╦╗╦╔═╗╔═╗  ╦═╗╔═╗╔╦╗╦╦═╗╔═╗╔═╗╔╦╗╔═╗   ┬   ╔╦╗╔═╗╦ ╦╔╗╔╦  ╔═╗╔═╗╔╦╗╔═╗
   //  ║║║║╚═╗║    ╠╦╝║╣  ║║║╠╦╝║╣ ║   ║ ╚═╗  ┌┼─   ║║║ ║║║║║║║║  ║ ║╠═╣ ║║╚═╗
